c4f4944ca4af8693cd42f3dc07bc7f2d31426c7a
BearerOP <ankity1892003@gmail.com>
2024-11-01 22:00:08 +0530

    deploy

---
+++ dist/index.js
@@ -0,0 +1,42 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require('dotenv').config();
+const express_1 = __importDefault(require("express"));
+const mintTokens_1 = require("./mintTokens");
+const app = (0, express_1.default)();
+app.use(express_1.default.json()); // Ensure you can parse JSON request bodies
+app.post('/helius', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const fromAddress = req.body.fromAddress;
+    const toAddress = req.body.toAddress;
+    const amount = req.body.amount;
+    const type = "received_native_sol"; // You may want to dynamically set this based on your application logic
+    try {
+        if (type === "received_native_sol") {
+            yield (0, mintTokens_1.mintTokens)({ fromAddress, toAddress, amount });
+        }
+        else {
+            yield (0, mintTokens_1.burnTokens)({ fromAddress, toAddress, amount });
+            yield (0, mintTokens_1.sendNativeTokens)({ fromAddress, toAddress, amount });
+        }
+        res.send('Transaction successful');
+    }
+    catch (error) {
+        console.error('Transaction failed:', error);
+        res.status(500).send('Transaction failed');
+    }
+}));
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
